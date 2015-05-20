0d52559ef19cc5b5ba447da64d0216ca5e260884
Martin Sturm <msturm@ebay.com>
2015-05-20 09:13:09 +0200

    Fix parsing issues

--- convert.py
+++ convert.py
@@ -2,17 +2,62 @@
 import csv
 import argparse
 import datetime
+import re
 
-parser = argparse.ArgumentParser(description='Convert csv to ofx')
-parser.add_argument("filename", help='csv filename to convert')
-args = parser.parse_args()
-
-
-def print_qif_header():
-    print "!Type:Bank"
 
 def fix_date(rawdate):
     return datetime.datetime.strptime(rawdate, "%Y%m%d").strftime("%d/%m/%Y")
+
+
+def parse_description(desc):
+    result = {}
+    if desc.startswith("BEA"):
+        result['name'] = desc[33:66].split(",")[0]
+        result['description'] = desc[99:166] if len(desc) > 99 else ''
+    elif desc.startswith("GEA"):
+        result['name'] = "Geldautomaat " + desc[33:66].split(",")[0]
+        result['description'] = ''
+    elif desc.startswith("/TRTP"):
+        result = parse_sepa(desc)
+    else:
+        result['name'] = desc[0:33].split(",")[0].strip()
+        result['description'] = desc[33:] if len(desc) >= 33 else ''
+    return result
+
+
+def parse_sepa(desc):
+    desc = remove_65_char_space(desc)
+    result = {}
+    if desc.startswith("/TRTP/SEPA OVERBOEKING") \
+            or desc.startswith("/TRTP/SEPA Incasso algemeen doorlopend") \
+            or desc.startswith("/TRTP/iDEAL"):
+        result['name'] = re.findall(" ?/NAME/ ?(.*?)/", desc)[0]
+        if desc.startswith("/TRTP/SEPA Incasso algemeen doorlopend"):
+            description_regex = re.findall("/REMI/(.*?)/IBAN", desc)
+        else:
+            description_regex = re.findall("/REMI/(.*?)/", desc)
+
+        if len(description_regex) > 0:
+            result['description'] = description_regex[0]
+        else:
+            result['description'] = ""
+    else:
+        print "Error while parsing " + desc
+        result['name'] = ''
+        result['description'] = ''
+    return result
+
+
+def remove_65_char_space(desc):
+    if len(desc) < 66:
+        return desc
+    elif desc[65] == ' ':
+        return desc[0:65] + remove_65_char_space(desc[66:])
+    elif desc[64] == ' ':
+        return desc[0:64] + remove_65_char_space(desc[65:])
+    elif desc[63] == ' ':
+        return desc[0:63] + remove_65_char_space(desc[64:])
+
 
 def format_name(desc):
     name = ""
@@ -43,25 +88,49 @@ def format_name(desc):
             name = desc[33:66]
     return name;
 
-def print_qif_stmt(date, amount, desc, memo=None):
-    number = ""
-    name = format_name(desc)
-    if memo == None:
-        memo = desc[99:166]
-    print "D{}".format(fix_date(date))
-    print "T{}".format(amount).replace(",", ".")
-    print "P{}".format(name)
-    if memo.strip() != "":
-        print "M{}".format(memo)
-    if number.strip() != "":
-        print "N{}".format(number)
-    print "^"
 
-with open(args.filename, 'rb') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter="\t")
-    print_qif_header()
-    counter = 1
+def print_qif_header():
+    return "!Type:Bank"
+
+
+def print_qif_stmt(data):
+    number = ""
+    name = data['name']
+    memo = data['description']
+    date = data['date']
+    amount = data['amount']
+    result = ""
+    result += "D{}\n".format(fix_date(date))
+    result += "T{}\n".format(amount).replace(",", ".")
+    result += "P{}\n".format(name)
+    if memo.strip() != "":
+        result += "M{}\n".format(memo)
+    if number.strip() != "":
+        result += "N{}\n".format(number)
+    result += "^\n"
+    return result
+
+
+def parse_file(csvreader):
+    result = []
     for row in csvreader:
-        #print "processing line: " + str(counter)
-        counter += 1
-        print_qif_stmt(row[2], row[6], row[7]) 
+        desc_line_result = parse_description(row[7])
+        result.append({'date': row[2], 'amount': row[6], 'description': desc_line_result['description'], 'name': desc_line_result['name']})
+    return result
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert csv to ofx')
+    parser.add_argument("filename", help='csv filename to convert')
+    args = parser.parse_args()
+
+    result = ""
+    with open(args.filename, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter="\t")
+        result = print_qif_header()
+        counter = 1
+        for row in parse_file(csvreader):
+            #print "processing line: " + str(counter)
+            counter += 1
+            result += print_qif_stmt(row)
+
+    print result
